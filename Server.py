23fa57689c3d720600b38ac5ac609aa929160217
mcshawn10 <shawnmcneill10@gmail.com>
2023-02-03 20:08:49 -0600

    update server.py

--- Server.py
+++ Server.py
@@ -1,13 +1,42 @@
 
 
 import socket
-from _thread import *
+import threading
 from ChessBoard import ChessBoard
 import pickle
 import time
 
+HEADER = 64
+PORT = 5050
 
 
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print("New Connection {addr} connected.")
+
+    connected = True
+    while connected:
+
+        msg = conn.recv()
+
+
+
+def start():
+    server.listen()
+
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+print("Server is starting...")
 if __name__ == "__main__":
 
     pass
